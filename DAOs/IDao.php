eb35b0b9559f9b8b5f192118736f7dd149dfa4bc
ecs90 <ecs_salas@hotmail.com>
2017-10-27 19:18:41 -0300

    ADD getAll()

--- DAOs/IDao.php
+++ DAOs/IDao.php
@@ -5,6 +5,7 @@ namespace DAOs;
 interface IDAO
 {
     public function agregar($modelo);
+    public function getAll();
     //public function eliminar($modelo);
     //public function buscar($modelo);
 }
