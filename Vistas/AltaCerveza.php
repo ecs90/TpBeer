5e6913a9f87e563598199adc3f113cf52a6b6bf3
mpmiceli <mariapaulamiceli@gmail.com>
2017-10-30 18:34:41 -0300

    nada

--- Vistas/AltaCerveza.php
+++ Vistas/AltaCerveza.php
@@ -7,23 +7,27 @@
 		<center>
 			<form action="/TpBeer/Cerveza/darDeAlta" method="post">
 				<label for="nombre">Nombre:</label>
-				<input type="text" id="nombre" name="nombre" >
+				<input type="text" id="nombre" name="nombre" size="30">
 				<br><br>
 				<label for="descripcion">Descripcion:</label>
-				<input type="text" id="descripcion" name="descripcion" >
+				<input type="text" id="descripcion" name="descripcion" size="30" >
 				<br><br>
 				<label for="precio">Precio:</label>
-				<input type="number" step="0.01" id="precio" name="precio" >
+				<input type="number" step="0.01" id="precio" name="precio" size="30">
 				<br><br>
 				<label for="stock">Stock:</label>
-				<input type="number" id="stock" name="stock" >
+				<input type="number" id="stock" name="stock" size="30" >
 				<br><br>
 				<label for="imagen">Imagen:</label>
-				<input type="file" id="imagen" name="imagen" >
+				<input type="file" id="imagen" name="imagen" size="30" >
 				<br><br>
 				<input type="submit" value="Cargar Cerveza">
 				<br><br>
 			</form>
-		</center>
+
+			<!--<div align="center" style="margin-top: 10px">
+				<a href="..\index.php">VOLVER A INICIO</a>
+			</div>
+		</center>-->
 	</body>
 </html>
