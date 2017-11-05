c7f8e88d1a980a3abfabcc39f1d16245d2045cc6
jonatanmena <menajonatan89@gmail.com>
2017-11-05 19:54:29 -0300

    pagina cliente

---
+++ Vistas/Cliente.php
@@ -0,0 +1,27 @@
+<?php namespace Vistas; 
+  require_once 'header.php';
+?>
+<body class="InitBody bg-img-adm">
+<div class="container-fluid admin-header">
+  <h1 class="display-4">Usuario</h1><br>
+</div>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+	<a class="navbar-brand" href="#">User</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+	    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle active" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Carrito</a>
+        <div class="dropdown-menu" aria-labelledby="dropdown01">
+          <a class="dropdown-item" href="/TpBeer/Pedido/listar">Mostrar</a>
+          <a class="dropdown-item" href="/TpBeer/Pedido/alta">Pedir</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+<?php  
+	require_once 'footer.php';
+?>
